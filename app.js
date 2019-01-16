0448f708270c872e8fcc6fe71768f77a9e30d45d
Udit Desai <udit.desai3@gmail.com>
2019-01-16 02:15:53 +0000

    add process vars

--- app.js
+++ app.js
@@ -16,8 +16,9 @@ var commentRoutes    = require("./routes/comments"),
     indexRoutes      = require("./routes/index");
 
 //configuration
-// mongoose.connect("mongodb://localhost:27017/yelp_camp", { useNewUrlParser: true });
-mongoose.connect("mongodb://udit:ursyd9375@ds157654.mlab.com:57654/yelp-camp", { useNewUrlParser: true });
+var url = process.env.DATABASEURL || "mongodb://localhost:27017/yelp_camp";
+mongoose.connect(url, { useNewUrlParser: true });
+//mongoose.connect("mongodb://udit:ursyd9375@ds157654.mlab.com:57654/yelp-camp", { useNewUrlParser: true });
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
